1cc505eabb0f9df62f7d38124835957597dc0354
Sivanishanth <sivanishanth303@gmail.com>
2026-06-09 12:11:04 +0530

    Fix API BaseURL to include /api

--- devconnect/frontend/src/api/axios.jsx
+++ devconnect/frontend/src/api/axios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API = axios.create({
-    baseURL : 'https://devconnect-503v.onrender.com'
+    baseURL : 'https://devconnect-503v.onrender.com/api'
 })
 
 export default API
