e1a93cbc8157bd34c82953e81add72d8c0c04ee6
Sivanishanth <sivanishanth303@gmail.com>
2026-06-09 11:50:55 +0530

    Updated API URL to Render Backend

--- devconnect/frontend/src/api/axios.jsx
+++ devconnect/frontend/src/api/axios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API = axios.create({
-    baseURL : 'http://localhost:5000/api'
+    baseURL : 'https://devconnect-503v.onrender.com'
 })
 
 export default API
--- devconnect/frontend/src/components/LoginRegisterStyle.jsx
+++ devconnect/frontend/src/components/LoginRegisterStyle.jsx
@@ -5,7 +5,20 @@ export default function LoginRegisterStyle({type,placeholder,value,onChange})
         placeholder={placeholder}
         value={value}
         onChange={onChange}
-        className="bg-gray-700 border border-gray-600 text-white rounded-lg px-4 py-3 w-full mb-4 focus:outline-none focus:border-b-black"/>
-    )
-}
+        className="
+        w-full
+        px-4 py-3
+        mb-4
+        rounded-lg
+        bg-gray-700
+        border border-gray-600
+        text-white
+        placeholder-gray-400
+        focus:outline-none
+        focus:ring-2
+        focus:ring-blue-500
+        transition
+      "
+    />
+)}
 
